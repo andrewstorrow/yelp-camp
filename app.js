57c9085d6cad877374c73cb3a3c68e16e6902f94
Andrew Storrow <andrew.storrow@gmail.com>
2018-01-04 17:29:08 -0600

    Restructure views folder. Continue work on Comments.

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     seedDB = require("./seeds");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost/yelp_camp")
+      .then(() => console.log(`Database connected`))
+      .catch(err => console.log(`Database connection error: ${err.message}`));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 seedDB();
@@ -23,7 +26,7 @@ app.get("/campgrounds", function(req, res){
        if(err) {
            
        } else {
-           res.render("index",{campgrounds:allCampgrounds});
+           res.render("campgrounds/index",{campgrounds:allCampgrounds});
        }
     });
 });
@@ -48,7 +51,7 @@ app.post("/campgrounds", function(req, res){
 
 //NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-   res.render("new") ;
+   res.render("campgrounds/new") ;
 });
 
 //SHOW - show info for one campground
@@ -59,9 +62,49 @@ app.get("/campgrounds/:id", function(req, res){
            console.log(err);
        } else {
             //render show template for that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
        }
    });
+});
+
+// ======================
+// COMMENTS ROUTES
+// ======================
+
+//NEW - show form to create new comment
+app.get("/campgrounds/:id/comments/new", function(req, res){
+  //find campground by id
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+//CREATE - add new comment to db and link it to a campground
+app.post("/campgrounds/:id/comments", function(req, res){
+  //Look up campground using ID
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      //Create new comment
+      Comment.create(req.body.comment, function(err, comment){
+        if(err) {
+          console.log(err);
+        } else {
+          //Connect new comment to campground
+          campground.comments.push(comment._id);
+          campground.save();
+          //Redirect back to campground page
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
 });
 
 app.listen("3000", function(){
--- seeds.js
+++ seeds.js
@@ -21,36 +21,43 @@ var data = [
 ];
 
 function seedDB(){
-	//Remove all campgrounds
-	Campground.remove({}, function(err){
-		if(err){
-			console.log(err);
-		}
-		console.log("removed campgrounds!");
-		//add a few campgrounds
-		data.forEach(function(seed){
-			Campground.create(seed, function(err, campground){
+	//Remove all comments
+	// Comment.remove({}, function(err){
+	// 	if(err){
+	// 		console.log(err);
+	// 	} else {
+			//Remove all campgrounds
+			Campground.remove({}, function(err){
 				if(err){
 					console.log(err);
-				} else {
-					console.log("added a campground");
-					Comment.create(
-					{
-						text: "This is place is great but I wish there was internet",
-						author: "Homer J Simpson"
-					}, function(err, comment){
+				}
+				console.log("removed campgrounds!");
+				//add a few campgrounds
+				data.forEach(function(seed){
+					Campground.create(seed, function(err, campground){
 						if(err){
 							console.log(err);
 						} else {
-							campground.comments.push(comment);
-							campground.save();
-							console.log("Created new comment");
+							console.log("added a campground");
+							Comment.create(
+							{
+								text: "This is place is great but I wish there was internet",
+								author: "Homer J Simpson"
+							}, function(err, comment){
+								if(err){
+									console.log(err);
+								} else {
+									campground.comments.push(comment._id);
+									campground.save();
+									console.log("Created new comment");
+								}
+							});
 						}
 					});
-				}
+				});
 			});
-		});
-	});
+	// 	}
+	// });
 }
 
 module.exports = seedDB;
